1ad0eccd87f3b43e3db4691771573236f9e241e8
Kayla Ramdeen <116173918+Kayla013@users.noreply.github.com>
2022-11-30 21:09:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+#import string
+#import random, os
+# clears system 
+#def clear():
+#  os.system("clear")
+#creates random pass
+#def create_password():
+#  all = string.ascii_letters +string.digits+ string.punctuation
+#password = random.choice(string.ascii_lowercase)
+#  password+= random.choice(string.ascii_uppercase)
+#  password+= #random.choice(string.digits)
+#password+=random.choice(string.punctuation)
+#for i in range(8):
+#    password+= random.choice(all)
+#  password_list= list(password)
+#  return password
+#print ("first random password is",create_password())
+
+
